c59d35846e206a2b3a93f1c022a5e51fb847c0e1
jenow <jenova.ke20@gmail.com>
2018-08-28 18:14:35 +0200

    exceptions

--- kuzzle.hpp
+++ kuzzle.hpp
@@ -2,27 +2,39 @@
 #define AUTH_HPP
 
 #include <string>
+#include <stdexcept>
 
 namespace kuzzleio {
-  class auth {
-    public:
+  struct auth {
     int toIgnore;
     int to_rename;
 
     void rename_me(const std::string& arg) {}
   };
 
-  class server {
-    public:
+  struct server {
     int rename_me;
 
     long now() {return 1535466635;}
   };
 
-  class listener {
-    public:
+  struct listener {
     virtual void trigger() = 0;
     virtual ~listener();
+  };
+
+  struct KuzzleException : std::runtime_error {
+    int status;
+
+    KuzzleException(int status=500, const std::string& message="Internal Exception");
+    KuzzleException(const KuzzleException& ke) 
+    : status(ke.status), std::runtime_error(ke.getMessage()) {};
+
+    virtual ~KuzzleException() throw() {};
+    std::string getMessage() const;
+  };
+  struct typemap {
+    void ICanThrow() throw(KuzzleException){}
   };
 }
 
--- swig.i
+++ swig.i
@@ -1,5 +1,6 @@
 %module(directors="1") kuzzlesdk
 %include "std_string.i"
+%include "exception.i"
 
 %extend kuzzleio::server {
   void hello() {
@@ -7,14 +8,27 @@
   }
 }
 
+
 %rename(Auth, match="class") auth;
 %ignore kuzzleio::auth::toIgnore;
 %rename(toRename) to_rename;
 // %rename(renameMe) rename_me;
 %rename(renameMe, fullname="1") kuzzleio::auth::rename_me(const std::string& arg);
 
+
 %javamethodmodifiers kuzzleio::auth::rename_me(const std::string& arg) "private";
 
+
 %feature("director") listener;
+
+%typemap(javabase) kuzzleio::KuzzleException "java.lang.RuntimeException";
+%typemap(throws,throws="io.kuzzle.sdk.KuzzleException") kuzzleio::KuzzleException {
+  (void)$1;
+  jclass excpcls = jenv->FindClass("io/kuzzle/sdk/KuzzleException");
+  if (excpcls) {
+    jenv->ThrowNew(excpcls, $1.what());
+   }
+  return $null;
+}
 
 %include "kuzzle.hpp"
